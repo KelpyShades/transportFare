aa553ada0cad6141ad2f1db923d24ac524495097
Kelvin Appiah <kelpyshades060904@gmail.com>
2023-09-25 23:31:32 +0000

    integrated arrays

--- main.cpp
+++ main.cpp
@@ -5,46 +5,50 @@
 
 using namespace std;
 
-int main()
-{
-    //Variables
+int main() {
+    // Constants and Arrays
     const float defaultFare = 2000.00;
+    float distances[] = {10.00, 40.00, 150.00, 1000.00};  // Upper limits of distance ranges
+    float fares[] = {defaultFare, 2100.00, 2175.00, 2050.00}; // Corresponding fares
+
+    //Variables
     float distance;
     float extra;
     float finalFare;
     cout << "...Transport Fare..." << endl;
 
     //Input
-    //While loop
     while (true) {
-    cout << "\nEnter Distance Travelled (in kilometers): ";
-    cin >>distance;
-    cout <<"Please Wait..."<< endl;
-    this_thread::sleep_for(chrono::seconds(5));//Delay
+        cout << "\nEnter Distance Travelled (in kilometers): ";
+        cin >> distance;
+        cout << "Please Wait..." << endl;
+        this_thread::sleep_for(chrono::seconds(5));
 
-    //Conditions
-    if (distance > 0 && distance <= 10.00) {
-        cout << "\nTransport Fare For A Distance Of "<<distance<<" KiloMeters Is: GH Cedis "<<fixed<<setprecision(2)<<defaultFare<< endl;
-        break;
-    }else if (distance > 10.00 && distance <= 40.00) {
-        extra = 100.00;
-        finalFare = extra + defaultFare;
-        cout << "\nTransport Fare For A Distance Of "<<distance<<" KiloMeters Is: GH Cedis "<<fixed<<setprecision(2)<<finalFare<< endl;
-        break;
-    }else if (distance > 40.00 && distance <= 150.00) {
-        extra = 175.00;
-        finalFare = extra + defaultFare;
-        cout << "\nTransport Fare For A Distance Of "<<distance<<" KiloMeters Is: GH Cedis "<<fixed<<setprecision(2)<<finalFare<< endl;
-        break;
-    }else if (distance > 150.00) {
-        extra = 50.00;
-        finalFare = extra + defaultFare;
-        cout << "\nTransport Fare For A Distance Of "<<distance<<" KiloMeters Is: GH Cedis "<<fixed<<setprecision(2)<<finalFare<< endl;
-        break;
-    }else {
-        cout << "\nEnter A Valid Amount Please Try Again" << endl;
-        continue;
+        //Conditions
+        if (distance > 0 && distance <= distances[0]) {
+            finalFare = fares[0];
+            cout << "\nTransport Fare For A Distance Of " << distance << " KiloMeters Is: GH Cedis " << fixed << setprecision(2) << finalFare << endl;
+            break;
+        } else if (distance <= distances[1]) {
+            extra = 100.00;
+            finalFare = fares[1];
+            cout << "\nTransport Fare For A Distance Of " << distance << " KiloMeters Is: GH Cedis " << fixed << setprecision(2) << finalFare << endl;
+            break;
+        } else if (distance <= distances[2]) {
+            extra = 175.00;
+            finalFare = fares[2];
+            cout << "\nTransport Fare For A Distance Of " << distance << " KiloMeters Is: GH Cedis " << fixed << setprecision(2) << finalFare << endl;
+            break;
+        } else if (distance <= distances[3]) {
+            extra = 50.00;
+            finalFare = fares[3];
+            cout << "\nTransport Fare For A Distance Of " << distance << " KiloMeters Is: GH Cedis " << fixed << setprecision(2) << finalFare << endl;
+            break;
+        } else {
+            cout << "\nEnter A Valid Amount. Please Try Again." << endl;
+            continue;
+        }
     }
-    }
+
     return 0;
 }
